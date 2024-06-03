5b8843ce84751c23b7d04ac5d83eccf7b75828d1
castanedadiaz <andresfcastaneda@yahoo.com>
2024-06-02 23:44:24 -0300

    subida 2

--- assets/js/scriptindex.js
+++ assets/js/scriptindex.js
@@ -3,3 +3,26 @@ function toggleRoomInfo(roomId) {
     roomInfo.style.display = roomInfo.style.display === "block" ? "none" : "block";
   }
   
+  document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('myModal');
+    var imgs = document.getElementsByClassName('gallery-img');
+    var modalImg = document.getElementById('img01');
+    var closeBtn = document.getElementsByClassName('close')[0];
+
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener('click', function () {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+        });
+    }
+
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
